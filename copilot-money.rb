0919bf423de81a9c4b61130fe44c4744e53745e2
Chandler Roth <hey@chandlerroth.com>
2023-05-08 09:27:36 -0400

    Add copilot-money

---
+++ copilot-money.rb
@@ -0,0 +1,10 @@
+cask "copilot-money" do
+  version :latest
+  sha256 :no_check
+
+  url "https://releases.copilot.money/latest"
+  name "Copilot"
+  homepage "http://copilot.money/"
+
+  app "Copilot.app"
+end
