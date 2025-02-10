069c3c15c4d8cf4afe978f9724b1373ee6b115de
Chandler Roth <hey@chandlerroth.com>
2025-02-10 10:47:30 -0500

    add scalar

---
+++ Casks/scalar.rb
@@ -0,0 +1,10 @@
+cask "scalar" do
+  version :latest
+  sha256 :no_check
+
+  url "https://download.scalar.com/mac/installer/universal"
+  name "Scalar"
+  homepage "https://scalar.com/"
+
+  app "Scalar.app"
+end
