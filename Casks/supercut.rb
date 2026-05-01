6c325591e4eb3aa19a03f776f83ba3c20f39758a
Chandler Roth <hey@chandlerroth.com>
2026-05-01 11:12:39 -0400

    Add supercut cask

---
+++ Casks/supercut.rb
@@ -0,0 +1,10 @@
+cask "supercut" do
+  version :latest
+  sha256 :no_check
+
+  url "https://macos.supercut.ai/Supercut.dmg"
+  name "Supercut"
+  homepage "https://supercut.ai/"
+
+  app "Supercut.app"
+end
