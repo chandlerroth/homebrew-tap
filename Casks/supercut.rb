fb114c287e4306895214505d4e35ab511af59679
Chandler Roth <hey@chandlerroth.com>
2026-07-24 17:03:20 -0500

    fix(cask): pin Supercut release

--- Casks/supercut.rb
+++ Casks/supercut.rb
@@ -1,10 +1,20 @@
 cask "supercut" do
-  version :latest
-  sha256 :no_check
+  version "1.0.892"
+  sha256 "d9c2669611d341ab6cced2098f5a619909a3351087a91c580e903ffd4efa0973"
 
-  url "https://macos.supercut.ai/Supercut.dmg"
+  url "https://macos.supercut.ai/Supercut-#{version}.app.zip"
   name "Supercut"
   homepage "https://supercut.ai/"
+
+  livecheck do
+    url "https://macos.supercut.ai/appcast.xml"
+    strategy :sparkle do |items|
+      items.find { |item| item.channel == "release" }&.short_version
+    end
+  end
+
+  auto_updates true
+  depends_on macos: :sonoma
 
   app "Supercut.app"
 end
